18a4622fa5dea205db91f440d8b1d3ec914e3b7d
epapak02 <epapak02@cs.ucy.ac.cy>
2016-06-18 19:51:25 +0300

    edit book

--- admin/edit_book/edit_book_form.php
+++ admin/edit_book/edit_book_form.php
@@ -12,12 +12,67 @@ $book_query = $conn->prepare("SELECT * FROM `books` WHERE book_id=".$_SESSION['b
 $book_query->execute();
 
 $book= $book_query->fetchAll(PDO::FETCH_ASSOC);
-if($book){
-    print_r($book);
-}else{
+$book=$book[0] ;
+if(!$book){
     print("error");
 }
 
-
 ?>
 
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+    <title>Hyperspace by HTML5 UP</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--[if lte IE 8]><script src="../assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="../assets/css/main.css" />
+    <!--[if lte IE 9]><link rel="stylesheet" href="../assets/css/ie9.css" /><![endif]-->
+    <!--[if lte IE 8]><link rel="stylesheet" href="../assets/css/ie8.css" /><![endif]-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script src="../assets/js/dataTables.js"></script>
+</head>
+<body>
+<!-- Sidebar -->
+<section id="sidebar">
+    <div class="inner" style="text-align: center">
+        <br> <br>
+        <?php echo '<img style="margin-bottom: 2%;" src="data:image/jpeg;base64,'.base64_encode( $book['Cover'] ).'"/>';?>
+        <a href="#" class="button special small">Αλλαγή Εξωφύλλου </a>
+        <br>
+        <?php echo '<img style="margin-bottom: 2%;" src="data:image/jpeg;base64,'.base64_encode( $book['Back_cover'] ).'"/>';?>
+
+        <a href="#" class="button special small">Αλλαγή Πισθοφύλλου</a>
+    </div>
+</section>
+
+<!-- Wrapper -->
+<div id="wrapper">
+    <section id="show_book" class="show_book" style="text-align: center">
+        <h2><?php echo $book['Title'];?></h2>
+        <?php while($element = current($book)) {
+            $key=key($book)."\n";
+            echo $key."\n";
+            next($book);
+        } ?>
+    </section>
+
+</div>
+
+
+<!-- Scripts -->
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/jquery.scrollex.min.js"></script>
+<script src="../assets/js/jquery.scrolly.min.js"></script>
+<script src="../assets/js/skel.min.js"></script>
+<script src="../assets/js/util.js"></script>
+<!--[if lte IE 8]><script src="../assets/js/ie/respond.min.js"></script><![endif]-->
+<script src="../assets/js/main.js"></script>
+
+</body>
+</html>
--- admin/index.php
+++ admin/index.php
@@ -75,16 +75,16 @@ if (session_status() == PHP_SESSION_NONE) {
 </section>
 
 <!-- Wrapper -->
-<div id="wrapper">
+    <div id="wrapper">
 
-    <!-- Λίστα Βιλίων -->
-    <!--<section id="intro" class="wrapper style1 fullscreen fade-up">-->
-    <!--<div class="inner">-->
-    <!--<h1>Hyperspace</h1>-->
-    <!--<p>Just another fine responsive site template designed by <a href="http://html5up.net">HTML5 UP</a><br />-->
-    <!--and released for free under the <a href="http://html5up.net/license">Creative Commons</a>.</p>-->
-    <!--<ul class="actions">-->
-    <!--<li><a href="#one" class="button scrolly">Learn more</a></li>-->
+        <!-- Λίστα Βιλίων -->
+        <!--<section id="intro" class="wrapper style1 fullscreen fade-up">-->
+        <!--<div class="inner">-->
+        <!--<h1>Hyperspace</h1>-->
+        <!--<p>Just another fine responsive site template designed by <a href="http://html5up.net">HTML5 UP</a><br />-->
+        <!--and released for free under the <a href="http://html5up.net/license">Creative Commons</a>.</p>-->
+        <!--<ul class="actions">-->
+        <!--<li><a href="#one" class="button scrolly">Learn more</a></li>-->
     <!--</ul>-->
     <!--</div>-->
     <!--</section>-->
