d9e4e0e4e289443c4a49e56b7555010245a6b4ea
akkous01 <akkous01@cs.ucy.ac.cy>
2016-06-29 18:18:39 +0300

    book profile done

---
+++ user/book_profile.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+
+
+  <header>
+    <ul class="my_nav">
+        <li><a href="#"><img src="images/navbar/1_HomePage.png"></a></li>
+        <li><a href="#"><img src="images/navbar/2_OurPhilosophy.png"></a></li>
+        <li><a href="#"><img src="images/navbar/3_Blog.png"></a></li>
+        <li><a href="#"><img src="images/navbar/4_Communicate.png"></a></li>
+        <li><a href="#"><img src="images/navbar/5_LogIn.png"></a></li>
+        <li><a href="#"><img src="images/navbar/6_CreateAccount.png"></a></li>
+        <li><a href="#"><img src="images/navbar/7_Gallery.png"></a></li>
+        <li><a href="#"><img src="images/navbar/8_ZoomIn.png"></a></li>
+    </ul>
+  </header>
+
+  <div id="main_book">
+    <div id="small_images">
+      <div id="cover"></div>
+      <div id="back_cover"></div>
+
+    </div>
+    <div id="show_image"></div>
+    <div id="main_characteristics"></div>
+  </div>
+  
+  <!-- <div id="table_img"></div> -->
+  
+  <div id="categories_nav">
+    <div id="ithiki_tab" class="categories_tab">
+      <img src="images/ithiki-tab.png" class="tab_img">
+    </div>
+    <div id="sindesi_tab" class="categories_tab">
+      <img src="images/sindesi-tab.png" class="tab_img">
+    </div>
+    <div id="epipleon_tab" class="categories_tab" >
+      <img src="images/epipleon-tab.png" class="tab_img">
+    </div>
+    <div id="gramatiki_tab" class="categories_tab">
+      <img src="images/grammatiki-tab.png" class="tab_img">      
+    </div>
+    <div id="analisi_tab" class="categories_tab">
+      <img src="images/analisi-tab.png" class="tab_img">
+    </div>
+  </div>
+
+  <div id="ithiki_box" class="categories_box">ithiki_box</div>
+  <div id="sindesi_box" class="categories_box" style="display:none;">sindesi_box</div>
+  <div id="epipleon_box" class="categories_box" style="display:none;">epipleon_box</div>
+  <div id="gramatiki_box" class="categories_box" style="display:none;">gramatiki_box</div>
+  <div id="analisi_box" class="categories_box" style="display:none;">analisi_box</div>
+
+
+   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/front-end.js"></script>
+</body>
+</html>
--- user/js/front-end.js
+++ user/js/front-end.js
@@ -66,7 +66,7 @@ $(document).ready(
 	    	}
 	    );
 
-	     $("#gramatiki").hover(
+	    $("#gramatiki").hover(
 	    	function(){
 	    		$("#ithika").show();
 	        	$("#sindesi").show();
@@ -82,6 +82,79 @@ $(document).ready(
 	        	
 	    	}
 	    );
+
+	    var top = $('header').offset().top - parseFloat($('header').css('marginTop').replace(/auto/, 0));
+    	$(window).scroll(function (event) {
+	        var y = $(this).scrollTop();
+	        //if y > top, it means that if we scroll down any more, parts of our element will be outside the viewport
+	        //so we move the element down so that it remains in view.
+	        if (y >= top) {
+	           var difference = y - top;
+	           $('header').css("top",difference);
+	       }
+   		});
+
+
+   		 $("#ithiki_tab").click(
+	    	function(){
+	    		$("#ithiki_box").show();
+	        	$("#sindesi_box").hide();
+	        	$("#epipleon_box").hide();
+	        	$("#gramatiki_box").hide();
+	        	$("#analisi_box").hide();
+	   
+	        	
+	    	}
+	    );
+
+   		  $("#sindesi_tab").click(
+	    	function(){
+	    		$("#ithiki_box").hide();
+	        	$("#sindesi_box").show();
+	        	$("#epipleon_box").hide();
+	        	$("#gramatiki_box").hide();
+	        	$("#analisi_box").hide();
+	   
+	        	
+	    	}
+	    );
+
+   		 $("#epipleon_tab").click(
+	    	function(){
+	    		$("#ithiki_box").hide();
+	        	$("#sindesi_box").hide();
+	        	$("#epipleon_box").show();
+	        	$("#gramatiki_box").hide();
+	        	$("#analisi_box").hide();
+	   
+	        	
+	    	}
+	    );
+
+   		 $("#gramatiki_tab").click(
+	    	function(){
+	    		$("#ithiki_box").hide();
+	        	$("#sindesi_box").hide();
+	        	$("#epipleon_box").hide();
+	        	$("#gramatiki_box").show();
+	        	$("#analisi_box").hide();
+	   
+	        	
+	    	}
+	    );
+
+   		 $("#analisi_tab").click(
+	    	function(){
+	    		$("#ithiki_box").hide();
+	        	$("#sindesi_box").hide();
+	        	$("#epipleon_box").hide();
+	        	$("#gramatiki_box").hide();
+	        	$("#analisi_box").show();
+	   
+	        	
+	    	}
+	    );
+
 
 
 	}
