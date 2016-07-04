34f316fc4c4c891ac95bb9dda3376491395aacb2
akkous01 <akkous01@cs.ucy.ac.cy>
2016-07-04 17:10:27 +0300

    new css files

--- user/book_profile.php
+++ user/book_profile.php
@@ -9,6 +9,7 @@
 
     <!-- Bootstrap -->
     <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/book_profile.css">
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
