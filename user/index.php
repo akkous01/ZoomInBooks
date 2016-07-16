9c84871109bce5d20cda0fd5ec943ab415269801
akkous01 <akkous01@cs.ucy.ac.cy>
2016-07-16 17:16:58 +0300

    carusel unfinished

--- user/index.php
+++ user/index.php
@@ -174,7 +174,45 @@ queries -->
 
     <div id="anakinosis">
       <div id="anakinosis_show">
-      	<?php echo $anakinosis_script; ?>
+
+		    <div id="carousel-example-generic" class="carousel slide" style="height:100%;width:100%" data-ride="carousel">
+			  <!-- Indicators -->
+			  <ol class="carousel-indicators">
+
+			  	
+			    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+			    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+			    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+			  </ol>
+
+			  <!-- Wrapper for slides -->
+			  <div class="carousel-inner" role="listbox">
+			    <div class="item active">
+			      <img class="carusel_img" src="images/dragon.png" alt="...">
+			      <div class="carousel-caption">
+			        ...
+			      </div>
+			    </div>
+			    <div class="item">
+			      <img src="..." alt="...">
+			      <div class="carousel-caption">
+			        ...
+			      </div>
+			    </div>
+			    ...
+			  </div>
+
+			  <!-- Controls -->
+			  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+			    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			    <span class="sr-only">Previous</span>
+			  </a>
+			  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+			    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			    <span class="sr-only">Next</span>
+			  </a>
+			</div>
+		    <!-- <?php echo $anakinosis_script; ?> -->
       </div>
     </div>
   </div>
